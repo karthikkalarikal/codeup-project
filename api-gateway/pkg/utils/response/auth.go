b5864cf848fd0145e0f1a757069635c2aef7fb5a
karthikkalarikal <karthikkalarikal009@gmail.com>
2024-03-02 23:30:54 +0530

    error with the mismatch of type was solved

--- api-gateway/pkg/utils/response/auth.go
+++ api-gateway/pkg/utils/response/auth.go
@@ -1,14 +1,16 @@
 package response
 
-import "database/sql"
+import (
+	"time"
+)
 
 type UserSignUpResponse struct {
-	ID        int            `json:"id"`
-	Username  sql.NullString `json:"username"`
-	Email     sql.NullString `json:"email"`
-	Password  string         `json:"-"` // Omit password in JSON responses
-	FirstName string         `json:"first_name"`
-	LastName  string         `json:"last_name"` // for proper representaion of null value in go sql.Null is used
-	CreatedAt sql.NullTime   `json:"created_at"`
-	UpdatedAt sql.NullTime   `json:"updated_at"`
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"` // Omit password in JSON responses
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"` // for proper representaion of null value in go sql.Null is used
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
