374583319ce5fab48812e393ccd40c98afeba430
karthikkalarikal <karthikkalarikal009@gmail.com>
2024-03-08 21:55:07 +0530

    refactoring go-sandbox

---
+++ sandbox-go/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// http.HandleFunc("/", handler)
+	// http.ListenAndServe(":80", nil)
+
+	
+}
---
+++ sandbox-go/pkg/executer/executor.go
@@ -0,0 +1,64 @@
+package executer
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type Executer struct {
+}
+
+func (e *Executer) GoCodeExec(request []byte, reply *[]byte) error {
+	// if r.Method != http.MethodPost {
+	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	// 	return
+	// }
+
+	// Read the submitted Go code
+	// code, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	http.Error(w, "Error reading code", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// Create a temporary directory
+	tempDir, err := os.MkdirTemp("", "gosandbox")
+	if err != nil {
+		// http.Error(w, "Error creating temporary directory", http.StatusInternalServerError)
+		return err
+	}
+	defer os.RemoveAll(tempDir) // Clean up the temporary directory
+
+	// Write the Go code to a temporary file
+	filePath := filepath.Join(tempDir, "main.go")
+	if err := os.WriteFile(filePath, request, 0644); err != nil {
+		// http.Error(w, "Error writing code to file", http.StatusInternalServerError)
+		return err
+	}
+
+	// Run the Go code
+	output, err := runGoCode(tempDir)
+	if err != nil {
+		// http.Error(w, fmt.Sprintf("Error running code: %s", err), http.StatusInternalServerError)
+		return err
+	}
+
+	*reply = output
+	return nil
+	// Send the output back to the user
+	// w.Header().Set("Content-Type", "text/plain")
+	// w.Write(output)
+}
+
+func runGoCode(directory string) ([]byte, error) {
+	cmd := exec.Command("go", "run", filepath.Join(directory, "main.go"))
+
+	// Capture the output of the command
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
